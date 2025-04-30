228b5473847c1945d6c44621282f2d31bfd5582e
mane <mane@manesec.com>
2025-05-01 03:42:43 +0800

    update

--- src/start.py
+++ src/start.py
@@ -5,4 +5,5 @@ def start_sign2n():
     script_path = Path(__file__).resolve().parent / "rsa_sign2n" / "standalone" / "jwt_forgery.py"
     args = sys.argv[1:]
     command = [sys.executable, str(script_path)] + args
+    os.chdir(Path(__file__).resolve().parent / "rsa_sign2n" / "standalone")
     subprocess.run(command)
